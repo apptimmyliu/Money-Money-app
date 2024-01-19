9f7d4e7680a889b2a0695d6e303f3823530afe3e
Timmy <timmyliu1997@hotmail.com>
2024-01-19 17:32:26 +0800

    Initial Commit

---
+++ Money Money app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Money app
+//
+//  Created by Timmy on 2024/1/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
