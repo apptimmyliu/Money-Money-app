0bbdb9d9f6acc4e6f43e9e134a6c773ff9200264
Timmy <timmyliu1997@hotmail.com>
2024-01-29 19:57:02 +0800

    update

--- Money Money app/ViewController.swift
+++ Money Money app/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import AVFoundation
 
+      
 class ViewController: UIViewController {
-
+    
+    
+    var player = AVPlayer()
+    
+    //顯示Money總數目的label
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    //點鈔次數
+    var money = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchButton(_ sender: Any) {
+        
+        //  加入音樂的程式碼
+        let url = Bundle.main.url(forResource: "sound", withExtension: "mp3")!
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+        //  按ㄧ次就會加100
+        money = money + 100
+        totalLabel.text = "\(money)"
+        
+        
+    }
+    //    重玩
+    @IBAction func restart(_ sender: Any) {
+        
+        money = 0
+        totalLabel.text = "0"
+        
+    }
 }
 
